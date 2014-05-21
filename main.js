19394ae47755ade4f77dbc618d247e4cb27a8d50
Trevor Allred <trevorallred@gmail.com>
2014-05-21 00:33:01 -0700

    add client files and get public files to display properly

--- main.js
+++ main.js
@@ -3,4 +3,10 @@
 var tantalimServer = require('tantalim-server/server');
 var app = tantalimServer.app;
 
+//var config = require('./config');
+var path = require('path');
+var pathToPublicDir = path.normalize(__dirname + '/node_modules/tantalim-client/public');
+
+tantalimServer.addClientFiles(pathToPublicDir);
+
 tantalimServer.start();

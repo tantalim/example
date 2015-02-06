600f2ecaf67890ec50635f69aa473e787c71f41c
Trevor Allred <trevorallred@gmail.com>
2015-02-06 15:47:11 -0800

    remove obsolete files

--- public/scratch/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('codePen', ['tantalim.common'])
-    .controller('Controller', function ($scope, ModelData) {
-        $scope.model = ModelData.model;
-        $scope.page = ModelData.page;
-        console.log(ModelData.model);
-    });

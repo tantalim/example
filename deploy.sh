e5374b631932da6904068fbeec36586aeadfd218
Trevor Allred <trevorallred@gmail.com>
2015-02-06 17:01:50 -0800

    copy deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+mysql -u root -e "DROP DATABASE IF EXISTS tantalum_meta; CREATE DATABASE tantalum_meta;"
+mysql -u root tantalum_meta < /home/tallred/tantalum_meta.sql
+
 APP_HOME=/usr/local
 TMP_DIR=/tmp
 
@@ -17,4 +20,6 @@ sudo rm -Rf ${APP_HOME}/example-master
 sudo mv ${TMP_DIR}/example-master ${APP_HOME}/
 cd ${APP_HOME}/example-master
 
-npm start
+cp ${APP_HOME}/example-master/deploy.sh /home/tallred/
+
+echo npm start
